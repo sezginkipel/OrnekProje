06c31cd5d145e95f1ee5ce58cae5edec4e10a6c7
Sezgin Kipel <1444sezgin@gmail.com>
2024-01-11 13:18:24 +0300

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {  
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
